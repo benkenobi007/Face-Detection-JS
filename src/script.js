4d8696ebeae229843d2431c99eda9d8cabf1dc05
benkenobi007 <ktiwari998@gmail.com>
2020-04-26 02:17:48 +0530

    Cropped face boundary

--- src/script.js
+++ src/script.js
@@ -1,4 +1,15 @@
 var video = document.getElementById('video')
+var container = document.getElementById('container')
+
+//set video and container dimensions
+// container.style.width = window.innerWidth + "px"
+// video.style.height = window.innerHeight + "px"
+// video.style.width = window.innerWidth + "px"
+var displaySize = {
+  width: video.width,
+  height: video.height
+}
+// container.style.height = window.innerHeight + "px"
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models')
@@ -20,12 +31,6 @@ function startVideo() {
 }
 
 video.addEventListener('play', () => {
-    //Resize the detection
-    const displaySize = {
-      width: video.width,
-      height: video.height
-    }
-
     const canvas = faceapi.createCanvasFromMedia(video)
     faceapi.matchDimensions(canvas, displaySize)
     document.body.append(canvas)
@@ -38,12 +43,35 @@ video.addEventListener('play', () => {
          const detections = await faceapi.detectAllFaces(video,
            new faceapi.SsdMobilenetv1Options())
            */
-      //console.log(detections)
-      const resizedDetections = faceapi.resizeResults(detections, displaySize)
-      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-      faceapi.draw.drawDetections(canvas, resizedDetections)
+      console.log('xmin : ' + detections._box._x)
+      console.log('ymin : ' + detections._box._y)
+      console.log('width : ' + detections._box._width)
+      console.log('height : ' + detections._box._height)
 
-    }, 100)
+      const resizedDetections = faceapi.resizeResults(detections, displaySize)
+
+      console.log('xmin : ' + resizedDetections._box._x)
+      console.log('ymin : ' + resizedDetections._box._y)
+      console.log('width : ' + resizedDetections._box._width)
+      console.log('height : ' + resizedDetections._box._height)
+
+      var detectX = resizedDetections._box._x
+      var detectY = resizedDetections._box._y
+      var detectWidth = resizedDetections._box._width
+      var detectHeight = resizedDetections._box._height
+
+      var croppedSquareLength = detectWidth > detectHeight ? detectWidth : detectHeight
+      container.style.width = croppedSquareLength + 50 + "px"
+      container.style.height = croppedSquareLength + 50 + "px"
+      container.style.borderRadius = "90%"
+
+      video.style.marginLeft = -detectX + "px"
+      video.style.marginTop = (-detectY + 50) + "px"
+
+      // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+      // faceapi.draw.drawDetections(canvas, resizedDetections)
+
+    }, 1000)
   }
 
 )
